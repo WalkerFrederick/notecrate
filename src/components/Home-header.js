e9767ee0cd636782d943d807381c61a07fe62235
Walker Frederick <walker@urdev.xyz>
2021-01-07 12:30:34 -0500

    video

--- src/components/Home-header.js
+++ src/components/Home-header.js
@@ -1,10 +1,17 @@
 import {Logo} from './index'
-
+import videoSrc from '../video.mp4'
+import poster from '../cratetest.jpg'
 
 function HomeHeader() {
   return (
     <div className="HomeHeader">
-        
+        <div className="button-container">
+            <button>SUBSCRIBE</button>
+            <button>CURRENT BOX</button>
+        </div>
+        <video autoPlay={true} muted loop poster={poster}>
+            <source src={videoSrc} type="video/mp4"/>
+        </video>
     </div>
   );
 }
